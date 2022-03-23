3525e84c7816f334a85d3eb816bc6d06c0a3a524
kosuke-17 <tamutamu17@icloud.com>
2022-03-24 08:31:47 +0900

    レスポンシブのハンバーガーメニューを表示

---
+++ src/app/components/navbar/menuStyles.ts
@@ -0,0 +1,46 @@
+export default {
+  bmBurgerButton: {
+    position: "absolute",
+    width: "20px",
+    height: "20px",
+    right: "20px",
+    top: "20px",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    postition: "fixed",
+    width: "60%",
+    height: "100%",
+    top: "0px",
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5rem 1.5rem 0",
+    fontSize: "1.15rem",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0,0,0,0.3)",
+  },
+};
--- src/app/components/navbar/navItems.tsx
+++ src/app/components/navbar/navItems.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+// typingディレクトリよりreact-burger-menuを
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyles";
 
 // ナビゲーションリスト全体のスタイル
 // list-none : リストのスタイルを無効にする
-const ListContainer = styled.div`
+const ListContainer = styled.ul`
   ${tw`
     flex
     list-none
@@ -11,7 +16,7 @@ const ListContainer = styled.div`
 `;
 
 // ナビゲーションのスタイル
-const NavItem = styled.div`
+const NavItem = styled.li`
   ${tw`
   text-xs
   md:text-base
@@ -27,6 +32,30 @@ const NavItem = styled.div`
 `;
 
 const NavItems = () => {
+  // 画面のサイズから真偽値を受け取る
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  // モバイルの場合、ハンバーガーメニューを利用
+  if (isMobile) {
+    return (
+      <Menu right styles={menuStyles}>
+        <ListContainer>
+          <NavItem>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">技術</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">ポートフォリオ</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">連絡先</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+  }
   return (
     <ListContainer>
       <NavItem>
