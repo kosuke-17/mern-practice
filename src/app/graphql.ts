f7c3e9bcbe7172f14f3036c3a7e43609d0b5383c
kosuke-17 <tamutamu17@icloud.com>
2022-04-05 09:25:39 +0900

    uri変更

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // rootのディレクトリとservices/index.tsで利用している
 export const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
+  uri: `${process.env.REACT_APP_API_URL}/graphql` as any,
   cache: new InMemoryCache(),
 });
