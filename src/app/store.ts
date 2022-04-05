4388216f2e564ecaa3673a79a505cb9c8ef3d989
kosuke-17 <tamutamu17@icloud.com>
2022-04-05 09:47:18 +0900

    reducのログを表示

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,16 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import homepageReducer from "./containers/HomePage/slice";
+import logger from "redux-logger";
 
+// storeの設定
 export const store = configureStore({
+  // reduxのログを表示する(prec state, action, next state)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   reducer: {
     homePage: homepageReducer,
   },
